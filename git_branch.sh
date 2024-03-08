c791971ab1b7b43b3d6fa0db7cf9651356460a6d
Oleg Karataev <kyrales@gmail.com>
2024-03-08 18:44:54 +0300

    Task-3

---
+++ git_branch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+                    git pull
+                    git checkout -B "master" "origin/master"
+                    git pull
+                    git checkout -B "branch_sync_hran" "origin/branch_sync_hran"
+                    logof=$(git log --reverse master...branch_sync_hran --pretty=format:"%h;%s|" | tr -d '\r\n')
+                    echo "Вывод лога: $logof"
+                    IFS='|' read -ra my_array <<< "$logof"
+                    echo "Вывод IFS: $IFS"
+                    for i in "${my_array[@]}"
+                        do
+                            BranchName=($(echo $i | sed 's/.*;//'))
+                            commit=($(echo $i | sed 's/;.*//'))
+                            git checkout -B "master" "origin/master"
+                            git checkout -B "feature/${BranchName}" "origin/feature/${BranchName}" || git checkout -B "feature/${BranchName}"
+                            git cherry-pick ${commit} --keep-redundant-commits --strategy-option recursive -X theirs
+                            git diff --name-only --diff-filter=U | xargs git rm -f
+                            git add .
+                            git commit -m "feature/${BranchName} - ${commit}"
+                            git push --set-upstream origin "feature/$i_branch"
+                            git rm 'src/cf/VERSION'
+                            git rm 'src/cf/dumplist.txt'
+                            git push origin "feature/${BranchName}"
+                        done
+                    git checkout -B "branch_sync_hran" "origin/branch_sync_hran"
+                    git merge "master"
+                    git push origin "branch_sync_hran"
+                    git checkout "master"
