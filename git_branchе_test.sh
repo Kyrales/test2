2411cfd24753290a35a37ee68df59c7cf185abf4
Oleg Karataev <kyrales@gmail.com>
2024-03-08 19:32:38 +0300

    Task-4 Система водопровода

---
+++ git_branchе_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+                    git pull
+                    git checkout -B "master" "origin/master"
+                    git pull
+                    git checkout -B "branch_sync_hran" "origin/branch_sync_hran"
+                    logof=$(git log --reverse master...branch_sync_hran --pretty=format:"%h;%s|" | tr -d '\r\n')
+                    echo "$0 Вывод лога: $logof"
+                    IFS='|' read -ra my_array <<< "$logof"
+                    echo "Вывод IFS: $IFS"
+                    for i in "${my_array[@]}"
+                        do
+                            BranchName=($(echo $i | sed 's/.*;//'))
+                            echo "$BranchName"
+                            commit=($(echo $i | sed 's/;.*//'))
+                            echo "$commit"
+                        done
